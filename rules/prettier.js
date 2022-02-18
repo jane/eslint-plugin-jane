1f1f8fd8c78db73aacb8ef55389450aa6aee27c2
Joseph Larson <larson.joseph@gmail.com>
2022-02-18 09:57:02 -0700

    Remove trivago plugin again [patch] (#63)
    * Remove trivago plugin again
    
    * 11.2.1-joe.0
    
    * 11.2.1

--- rules/prettier.js
+++ rules/prettier.js
@@ -6,7 +6,4 @@ module.exports = {
   tabWidth: 2,
   trailingComma: 'es5',
   quoteProps: 'preserve',
-  importOrder: ['^@janecorp/(.*)$', '^(@/|@:)(.+)$', '^[./]'],
-  importOrderSeparation: true,
-  importOrderSortSpecifiers: true,
 }
